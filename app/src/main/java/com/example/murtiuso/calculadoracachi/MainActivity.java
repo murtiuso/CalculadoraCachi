22e690e9d40ecd40be621e7ad8f414a2498435d7
Sergio Asuero Díaz <murtiuso@MBPro2017.local>
2017-10-26 08:34:07 +0200

    Antes de las modificaciones que aplico al seguir un buen manual

--- app/src/main/java/com/example/murtiuso/calculadoracachi/MainActivity.java
+++ app/src/main/java/com/example/murtiuso/calculadoracachi/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.murtiuso.calculadoracachi;
 
+import android.renderscript.Double2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText pantalla;
+    public Double2 operando1, operando2, resultado;
+    public int op;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pantalla = (EditText) findViewById(R.id.PantallaCalc);
     }
+
+    public void bt1 (View v){
+        String captura = pantalla.getText().toString();
+        captura = captura+"1";
+        pantalla.setText(captura);
+    }
+
+
 }
